ad6c001444375035e5b410b67079722568cb3283
Mike Hayes <mike.hayes10@gmail.com>
2018-01-12 03:11:35 +0000

    First commit.

---
+++ README.md
@@ -0,0 +1,15 @@
+# WHMCS Notifications Provider for Pushover
+
+Enables [Pushover](https://pushover.net) to be used with the [WHMCS](https://www.whmcs.com) notifications system. More information can be found on [this page](https://docs.whmcs.com/Notifications) and developer docs are available [here](https://developers.whmcs.com/notification-providers/).
+
+## Installation
+
+1. Create the directory `/modules/notifications/Pushover` within your WHMCS installation.
+2. Copy the contents of this repo's `src` directory to the above created directory.
+3. Configure via the Setup => Notifications page within the WHMCS admin control panel.
+
+## Changelog
+
+### 2018-01-12 1.0.0
+
+The first release, Hello World!
---
+++ src/Pushover.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace WHMCS\Module\Notification\Pushover;
+
+use WHMCS\Module\Contracts\NotificationModuleInterface;
+use WHMCS\Module\Notification\DescriptionTrait;
+use WHMCS\Notification\Contracts\NotificationInterface;
+
+class Pushover implements NotificationModuleInterface
+{
+    use DescriptionTrait;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->setDisplayName('Pushover');
+        $this->setLogoFileName('logo.png');
+    }
+
+    /**
+     * Provider settings.
+     *
+     * @return array
+     */
+    public function settings()
+    {
+        return [
+            'api_token' => [
+                'FriendlyName' => 'API Token',
+                'Type' => 'password',
+                'Description' => 'Your Pushover application token.',
+            ],
+        ];
+    }
+
+    /**
+     * Test connection.
+     *
+     * @param array $settings
+     * @return array
+     */
+    public function testConnection($settings)
+    {
+        // Do nothing.
+        // Pusher doesn't provide a way to validate the API token alone.
+    }
+
+
+    /**
+     * Individual notification settings.
+     *
+     * @return array
+     */
+    public function notificationSettings()
+    {
+        return [
+            'user' => [
+                'FriendlyName' => 'User/Group Key',
+                'Type' => 'text',
+                'Description' => 'The recipient key, obtained from the Pushover dashboard.',
+            ],
+            'device' => [
+                'FriendlyName' => 'Device Name (Optional)',
+                'Type' => 'text',
+                'Description' => 'The recipient\'s device name. Multiple devices can be separated by a comma or leave blank for all devices.',
+            ],
+            'sound' => [
+                'FriendlyName' => 'Sound',
+                'Type' => 'dynamic',
+                'Description' => 'Choose the sound for this notification.',
+            ],
+            'priority' => [
+                'FriendlyName' => 'Priority',
+                // TODO: Replace with the 'dropdown' type once WHMCS issue #MODULE-6698 is resolved.
+                'Type' => 'dynamic',
+                'Description' => 'Choose the priority for this notification. Emergency priority notifications will require acknowledgement.',
+            ],
+            'retry' => [
+                'FriendlyName' => 'Retry Period',
+                'Type' => 'text',
+                'Description' => 'Emergency priority only. Defines the number of seconds Pushover will wait between re-delivery attempts of the same notification until it\'s acknowledged or expires (see Expires setting). The value must be a minimum of 30.',
+            ],
+            'expires' => [
+                'FriendlyName' => 'Expires',
+                'Type' => 'text',
+                'Description' => 'Emergency priority only. Defines the number of seconds Pushover will continue to attempt re-delivery of the same notification before giving up. The value can be a maximum of 10800 (3 hours).',
+            ],
+        ];
+    }
+
+    /**
+     * Get dynamic field configuration.
+     *
+     * @param string $fieldName
+     * @param array $settings
+     * @return array
+     */
+    public function getDynamicField($fieldName, $settings)
+    {
+        switch ($fieldName) {
+            case 'sound':
+                $response = json_decode(file_get_contents('https://api.pushover.net/1/sounds.json?token=' . $settings['api_token']), true);
+                
+                $values = array_map(function ($name, $id) {
+                    return compact('name', 'id');
+                }, $response['sounds'], array_keys($response['sounds']));
+
+                return ['values' => $values];
+            case 'priority':
+                return [
+                    'values' => [
+                        ['id' => '-2', 'name' => 'Lowest'],
+                        ['id' => '-1', 'name' => 'Low'],
+                        ['id' => '0',  'name' => 'Normal'],
+                        ['id' => '1',  'name' => 'High'],
+                        ['id' => '2',  'name' => 'Emergency'],
+                    ],
+                ];
+            default:
+                throw new Exception("The field name '{$fieldName}' is not recognised.");
+        }
+    }
+
+    /**
+     * Build the Pushover message including the original message and attributes.
+     *
+     * @param NotificationInterface $notification
+     * @return string
+     */
+    private function buildMessage(NotificationInterface $notification)
+    {
+        $message = $notification->getMessage() . PHP_EOL . PHP_EOL;
+
+        foreach ($notification->getAttributes() as $attribute) {
+            $message .= $attribute->getLabel() . ': ' . $attribute->getValue() . PHP_EOL;
+        }
+
+        return $message;
+    }
+
+    /**
+     * Get dynamic field configuration.
+     *
+     * @param NotificationInterface $notification
+     * @param array $moduleSettings
+     * @param array $notificationSettings
+     * @return array
+     */
+    public function sendNotification(NotificationInterface $notification, $moduleSettings, $notificationSettings)
+    {
+        $data = [];
+        $data['token'] = $moduleSettings['api_token'];
+        $data['user'] = $notificationSettings['user'];
+        $data['title'] = $notification->getTitle();
+        $data['message'] = $this->buildMessage($notification);
+        $data['priority'] = array_shift(explode('|', $notificationSettings['priority']));
+        $data['sound'] = array_shift(explode('|', $notificationSettings['sound']));
+
+        if (!empty($notificationSettings['device'])) {
+            $data['device'] = $notificationSettings['device'];
+        }
+
+        if ($data['priority'] == 2) {
+            $data['retry'] = empty($notificationSettings['retry']) ? $notificationSettings['retry'] : 300;
+            $data['expire'] = empty($notificationSettings['expires']) ? $notificationSettings['expires'] : (3600 * 10);
+        }
+
+        $ch = curl_init('https://api.pushover.net/1/messages.json');
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+        curl_exec($ch);
+        curl_close($ch);
+    }
+}
